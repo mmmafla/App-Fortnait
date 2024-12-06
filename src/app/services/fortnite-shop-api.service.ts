ac0299c3d1eef8ec3198914728a3118ce0faa2e9
benjamin <133883483+mmmafla@users.noreply.github.com>
2024-12-06 00:04:59 -0300

    PT1

--- src/app/services/fortnite-shop-api.service.ts
+++ src/app/services/fortnite-shop-api.service.ts
@@ -11,18 +11,19 @@ import { Observable } from 'rxjs';
 
 export class FortniteShopApiService {
 
-  private apiUrl = 'https://fortnite-api.com/v2/shop';
+  private apiUrl = 'https://fortniteapi.io/v2/shop';
   private key = 'afc78907-1dd3-428b-b17a-dd4ed8b161c3';
   private newsApiUrl = 'https://fortnite-api.com/v2/news';
   private torneos = 'https://fortniteapi.io/v1/events/list?lang=es&region=BR&season=current';
   private keytorneos = '5a8f26a2-3d756271-eed41ad1-144fe5e0';
+  private mapa = 'https://fortnite-api.com/v1/map';
 
   constructor(private http: HttpClient) { }
 
   getShopItems(): Observable<any> {
     return this.http.get<any>(this.apiUrl, {
       headers: {
-        'Authorization': this.key
+        'Authorization': this.keytorneos
       }
     });
   }
@@ -46,5 +47,14 @@ export class FortniteShopApiService {
       },
     });
   }
-
+  getMap(): Observable<any>{
+    return this.http.get<any>(this.mapa,{
+      headers: {
+        'Authorization': this.key,
+      },
+      params:{
+        'language': 'es',
+      }
+    })
+  }
 }
